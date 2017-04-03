ad71276aec6beb7106fd43abb96d493dc505deb7
louder1983 <louder1983@gmail.com>
2017-04-03 13:50:32 -0500

    Add code Apr 3th 2017

--- LeetCode_Java/src/com/leetcode/java/packages41_60/PS_60.java
+++ LeetCode_Java/src/com/leetcode/java/packages41_60/PS_60.java
@@ -1,5 +1,8 @@
 package com.leetcode.java.packages41_60;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class PS_60 {
 
 	/*** 60. Permutation Sequence ***/
@@ -27,14 +30,35 @@ public class PS_60 {
 	public static void main(String[] args){
 		
 		String nums = "";
-		int n = 9;
-		int k = 2;
+		int n = 3;
+		int k = 4;
 		
 		nums = getPermutation(n, k);	
 		System.out.println(nums);
 	}
 	
+	public static final int[] orders = {1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
     public static String getPermutation(int n, int k) {
-        
+    	List<Integer> numbers = new LinkedList<Integer>();
+    	for(int i=0; i<n; i++){
+    		numbers.add(i+1);
+    	}
+    	if(k > orders[n-1]) return "";
+    	
+    	StringBuffer permutation = new StringBuffer();
+    	int remain = k-1;
+    	for(int i=n-1; i>=0; i--){
+    		int value;
+    		if(i!=0){
+	    		value = remain / orders[i-1];
+	    		remain = remain % orders[i-1];
+    		} else {
+    			value = remain;
+    		}
+    		char add = (char)('0' + numbers.get(value));
+    		numbers.remove(value);
+    		permutation.append(add);
+    	}
+    	return permutation.toString();
     }
 }
